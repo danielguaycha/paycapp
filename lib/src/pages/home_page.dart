f70e2dd17fadd7d5bfb2bd05696fc1b55a936164
Daniel Guaycha <danielguaycha@gmail.com>
2020-04-16 16:58:26 -0500

    Arreglo en la buttomBar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -20,40 +20,83 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
   final prefs = new LocalStorage();
-  TabController _tabController;
+  int _currentIndex = 0;
+  PageController _pageController;
+
+  final List<Widget> _children = [
+    IndexPage(),
+    ListCreditPage(),
+    ShowPaymentsPage(),
+    ListExpensePage(),
+    UserPage(),
+  ];
+
   @override
   void initState() {  
     super.initState();
-    _tabController = new TabController(length: 5, initialIndex: 0, vsync: this);
+    _pageController = PageController();
   }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        physics: NeverScrollableScrollPhysics(),
-        controller: _tabController,
-        children: <Widget>[
-          IndexPage(),
-          ListCreditPage(),
-          ShowPaymentsPage(),
-          ListExpensePage(),
-          UserPage(),
-        ],
-      ),
-      bottomNavigationBar: new Material(      
-        color: Theme.of(context).primaryColor,      
-        child: new TabBar(
-          isScrollable: false,                    
-          tabs: <Widget>[
-            new Tab(icon: Icon(FontAwesomeIcons.home)),
-            new Tab(icon: Icon(Icons.payment)),
-            new Tab(icon: Icon(Icons.pages)),
-            new Tab(icon: Icon(Icons.monetization_on)),
-            new Tab(icon: Icon(Icons.settings)),
-          ],
-          controller: _tabController,
+      //body: _children[_currentIndex], // new
+      body: SizedBox.expand(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          children: _children
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Colors.orangeAccent,
+        elevation: 12,
+        unselectedItemColor: Colors.white70,
+        type: BottomNavigationBarType.fixed,
+        onTap: onTabTapped, // new
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex, // new
+        items:<BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.payment),
+            title: new Text('Créditos'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.pages),
+              title: Text('Cobros')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.monetization_on),
+              title: Text('Gastos')
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Usuario')
+          )
+        ],
+      ),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+      _pageController.animateToPage(index,
+          duration: Duration(milliseconds: 100), curve: Curves.decelerate);
+    });
   }
 }
